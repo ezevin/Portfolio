0a81a6702e4bbd01d01d21919a48f0249954d6b4
ezevin <ehzevin@gmail.com>
2019-08-24 13:06:23 -0400

    resume page now has work and education history

--- src/Containers/Resume.js
+++ src/Containers/Resume.js
@@ -11,9 +11,87 @@ class Resume extends Component{
           <h1>Resume</h1>
         </center>
         <div className="text">
-          <h2>Skills:</h2>
+          <h2>Technical Skills:</h2>
           <p>
-            Ruby (on Rails), HTML, CSS, SQL, JavaScript, React.js, AutoCAD, Photoshop, Microsoft Office
+            <ul>
+              <li>Ruby (on Rails)</li>
+              <li>HTML/CSS</li>
+              <li>SQL</li>
+              <li>JavaScript ES6</li>
+              <li>React.js</li>
+              <li>Semantic UI</li>
+              <li>Bootstrap</li>
+              <li>Heroku</li>
+              <li>Git</li>
+              <li>AutoCAD</li>
+              <li>Photoshop</li>
+              <li>Microsoft Office</li>
+          </ul>
+
+          </p>
+          <h2>Employment History:</h2>
+          <p>
+            <span className="bold">
+              Freelance Pyrotechnician, New York City, New York
+              <br />
+              Pyrotechnician, Fall 2016 - Present
+              <br />
+            </span>
+            <span className="italics">
+              Signature Theater, Playwrights Horizons, Forged In Fire  (Television)
+            </span>
+            <br />
+              +Responsible for lead set up and running effects for multiple productions
+            <br />
+              +Worked to maintain sustainability and reusability of effects
+            <br />
+              +Monitored fire and pyrotechnics on stage and set
+            <br />
+              +Maintained OSHA and FDNY safety standards for both actors and crew
+            <br /><br />
+            <span className="bold">
+              Prop n Spoon, Rahway, New Jersey
+              Props Artisan, Fall 2018
+            </span>
+            <br />
+              +Researched props and scenic elements for commercials, theater, film
+              <br />
+              +Built and painted props and scenic elements for commercials, theater, film
+            <br /> <br />
+            <span className="bold">
+              J&M Special Effects, Brooklyn, New York
+              <br />
+              FX Technician, 2016 - 2018
+            </span>
+            <br />
+              +Created special effects for commercials, theater, film, television, print ads
+              <br />
+              +Lead confetti, pyrotechnics, wind, water, fire, snow, and blood effects
+              <br />
+              +Repaired, maintained, and procured equipment used to create special effects
+              <br />
+          </p>
+          <h2>Education:</h2>
+          <p>
+            <span className="bold">
+              Flatiron School - New York, NY - March 2019 - June, 2019
+            </span>
+            <br />
+              +Full Stack Web Development, Ruby on Rails, JavaScript, React.js
+            <br /> <br />
+            <span className="bold">
+              Yale School of Drama - New Haven, CT - Class of 2012
+            </span>
+            <br />
+              +Technical Internship Certificate
+              <br />
+              +Properties Internship Certificate
+            <br /> <br />
+            <span className="bold">
+              Emerson College - Boston, MA - Class of 2011
+            </span>
+            <br />
+              +B.F.A. Design Technology for Theater (Cum Laude)
           </p>
         </div>
       </div>
