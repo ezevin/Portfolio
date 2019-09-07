f5ccb324987c243f893bd063ebe414579fdc7750
ezevin <ehzevin@gmail.com>
2019-09-07 11:55:26 -0400

    cleaned up reveal mess

--- src/Containers/TitleContainer.js
+++ src/Containers/TitleContainer.js
@@ -7,10 +7,7 @@ class TitleContainer extends Component{
 
     return(
       <>
-      <div className="title box-slide reveal-top">
-        <div className="reveal-content">
-          <h1>Hello</h1>
-        </div>
+      <div className="title ">
         <center>
           <br />
           <img className="responsive profileimg" src={Profile} alt="Avatar"  />
@@ -39,7 +36,8 @@ class TitleContainer extends Component{
       </div>
   <div class="box-slide">
      <div class="reveal-content">
-       <h1>Hello</h1>
+       <p className="bioText">Full Stack Web Developer with a passion for creativity and critical thinking with experience in Ruby on Rails, JavaScript, and React.js. Skilled at team-building, project management, and finding creative solutions to complex problems.
+       Previous experience includes technical entertainment/theater and special effects/pyrotechnics. Pursued web development for the stimulating challenge and the joy of magically transforming visions to reality that it provides.</p>
      </div>
     <div class="reveal-top">
       <div class="reveal-content caption">
