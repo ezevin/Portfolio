b511cce73111e390722a4a4ea64f28df7d57dc18
ezevin <ehzevin@gmail.com>
2019-09-12 16:23:47 -0400

    Updates

---
+++ precache-manifest.8f3529da8f9566a9b377b733feadaf19.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39f3aa72728ef2c405fb141caa292fe1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e630ef689f5bbf4601d6",
+    "url": "/portfolio/static/css/main.dc7cc1bd.chunk.css"
+  },
+  {
+    "revision": "7118081e6f244b476982",
+    "url": "/portfolio/static/js/2.14f2e97a.chunk.js"
+  },
+  {
+    "revision": "e630ef689f5bbf4601d6",
+    "url": "/portfolio/static/js/main.e113e8e1.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "890c3b6513fd8c5e3b7b5376099a603a",
+    "url": "/portfolio/static/media/Background.890c3b65.png"
+  },
+  {
+    "revision": "e0e523f3fd04f01aa4dc2da73e5537da",
+    "url": "/portfolio/static/media/Book.e0e523f3.png"
+  },
+  {
+    "revision": "7f9184ac13503ec0eb337b1a4f3acb1c",
+    "url": "/portfolio/static/media/Fabfolio.7f9184ac.png"
+  },
+  {
+    "revision": "601af19fec753f3ea2ae3fde13759143",
+    "url": "/portfolio/static/media/Option3.601af19f.jpg"
+  },
+  {
+    "revision": "b877acd423cae53bdc0f4e1339867370",
+    "url": "/portfolio/static/media/Pawfect.b877acd4.png"
+  },
+  {
+    "revision": "0812275495c08171b37b3397c0f3d846",
+    "url": "/portfolio/static/media/Profile.08122754.jpg"
+  }
+]);
