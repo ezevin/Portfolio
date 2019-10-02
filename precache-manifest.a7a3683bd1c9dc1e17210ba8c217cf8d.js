b5d3c6f8ed27447aec05d220b245e657cc12d001
ezevin <ehzevin@gmail.com>
2019-10-02 12:23:26 -0400

    Updates

---
+++ precache-manifest.a7a3683bd1c9dc1e17210ba8c217cf8d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae8c72e5f45aff76dd48230115bcab17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18516889e001ce72cc00",
+    "url": "/static/css/main.73e9768d.chunk.css"
+  },
+  {
+    "revision": "0dc046e86a2d754027c2",
+    "url": "/static/js/2.37d85aa7.chunk.js"
+  },
+  {
+    "revision": "18516889e001ce72cc00",
+    "url": "/static/js/main.41292002.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "890c3b6513fd8c5e3b7b5376099a603a",
+    "url": "/static/media/Background.890c3b65.png"
+  },
+  {
+    "revision": "e0e523f3fd04f01aa4dc2da73e5537da",
+    "url": "/static/media/Book.e0e523f3.png"
+  },
+  {
+    "revision": "7f9184ac13503ec0eb337b1a4f3acb1c",
+    "url": "/static/media/Fabfolio.7f9184ac.png"
+  },
+  {
+    "revision": "b877acd423cae53bdc0f4e1339867370",
+    "url": "/static/media/Pawfect.b877acd4.png"
+  },
+  {
+    "revision": "0812275495c08171b37b3397c0f3d846",
+    "url": "/static/media/Profile.08122754.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.90ca4fe5b2d00725eb2fbd8b1ebafec6.js"
+  "/precache-manifest.a7a3683bd1c9dc1e17210ba8c217cf8d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
